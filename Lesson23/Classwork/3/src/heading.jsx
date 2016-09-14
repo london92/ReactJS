776cffc0087bfba45f50756301aa99413020b491
Nikolai Chmyr <nikolai.chmyr@gmail.com>
2016-09-14 22:20:31 +0300

    lesson23 cw ex 3

---
+++ Lesson23/Classwork/3/src/heading.jsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Heading extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <h1>Hello from heading.jsx with browserify help</h1>
+        )
+    }
+}
+export {Heading}
---
+++ Lesson23/Classwork/3/src/paragraph.jsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Paragraph extends React.Component{
+    constructor(){
+        super();
+    }
+    render(){
+        return(
+            <p>Hello from paragraph.jsx</p>
+        )
+    }
+}
+export {Paragraph}
